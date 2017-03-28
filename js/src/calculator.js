35fb3f051cc0acf5bbdf35ec2699c8b7b8382316
James Pederson <james@jpederson.com>
2017-03-27 21:37:02 -0500

    Add styles to result divs and default instructions.

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -1,63 +1,35 @@
 
 
 $(function(){
-	
-	var recalculate_total = function(){
-			var total = parseFloat( typeof( $(".result.credit").html() )!=="undefined" ? $(".result.credit").html().replace("$","").replace(",","") : 0 )+
-				parseFloat( typeof( $(".result.loan-auto").html() )!=="undefined" ? $(".result.loan-auto").html().replace("$","").replace(",","") : 0 )+
-				parseFloat( typeof( $(".result.loan-personal").html() )!=="undefined" ? $(".result.loan-personal").html().replace("$","").replace(",","") : 0 );
-			if ( total>0 ) {
-				$(".result.total").html(""+total.toFixed(2));
-			}
-		};
 
 	$(".calculator.credit").accrue({
 		mode: "compare",
 		response_output_div: ".result.credit",
-		response_compare:"Payment: $%loan_2_payment_amount%/mo<br>Savings: $%savings%",
-		error_text:"Enter loan info to see payment and savings.",
-		callback: function( elem, data ){
-			if ( data!==0 ) {
-				recalculate_total();
-			}
-		}
+		response_compare:"<strong>Payment: $%loan_2_payment_amount%/mo<br>Savings: $%savings%</strong>",
+		error_text:"<span>Enter loan info to see payment and savings.</span>"
 	});
 
 	$(".calculator.loan-auto").accrue({
 		mode: "compare",
 		response_output_div: ".result.auto",
-		response_compare:"Payment: $%loan_2_payment_amount%/mo<br>Savings: $%savings%",
-		error_text:"Enter loan info to see payment and savings.",
+		response_compare:"<strong>Payment: $%loan_2_payment_amount%/mo<br>Savings: $%savings%</strong>",
+		error_text:"<span>Enter loan info to see payment and savings.</span>",
 		callback: function( elem, data ){
-			if ( data!==0 ) {
-				recalculate_total();
-			}
+			console.log( elem );
+			console.log( data );
 		}
 	});
 
 	$(".calculator.loan-personal").accrue({
 		mode: "compare",
 		response_output_div: ".result.personal",
-		response_compare:"Payment: $%loan_2_payment_amount%/mo<br>Savings: $%savings%",
-		error_text:"Enter loan info to see payment and savings.",
-		callback: function( elem, data ){
-			if ( data!==0 ) {
-				recalculate_total();
-			}
-		}
+		response_compare:"<strong>Payment: $%loan_2_payment_amount%/mo<br>Savings: $%savings%</strong>",
+		error_text:"<span>Enter loan info to see payment and savings.</span>"
 	});
 
 	$(".numbers-only").keyup(function(){
 		var fixed=$(this).val().replace(/[^0-9.]/g,"");
 		$(this).val( fixed );
-	});
-
-	$(".calculate").click(function(){
-		$(".tool").slideUp( 'slow' );
-		$(".results").slideDown( 'slow' );
-		$('html,body').animate({
-			scrollTop: $( "header img" ).height()
-        }, 800);
 	});
 
 	$(".go-back").click(function(){
